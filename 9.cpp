656af0b7fad5544e52cd1e7f1a1d88b0b3a67c4b
Uttam-Sarkar <s2010876149ac.ru.bd>
2023-11-03 02:20:11 +0600

    update 9 no code answer

--- 9.cpp
+++ 9.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 string ans="";
-string input;
+vector <string> inputV;
+int ii=0;
 
 int chomoskyNormalForm(string s){
-	//cout << s << " " << input << endl;
 	if(s == "PN" || s == "P" || s == "N" || s == "V" ){
 		 ans = s;
 	}
-	else if(s == input){
+	else if(s == inputV[ii]){
+		ii++;
 		return 1;
 	}
 	return 0;
@@ -19,33 +20,50 @@ int main()
 	// Opening the file
 	ifstream file;
 	file.open("9.txt");
+	string input;
+	getline(cin, input);
+	int len = input.size();
+	string str = "";
 
-	cin >> input;
-
-	string str;
+	for(int i=0; i<len; i++){
+		if(input[i]!=' '){
+			str = str+input[i];
+		}
+		else{
+			inputV.push_back(str);
+			str = "";
+		}
+	}
+	if(str.size()){
+		inputV.push_back(str);
+	}
+	int countt = inputV.size();
+	str = "";
+	
 	cout << "Output: ";
 
 	int check=0;	
-	vector<string> v;;
+	vector<string> v;
 
 	while (file >> str) {
 		check = chomoskyNormalForm(str);
 		if(check){
 			v.push_back(ans);
-			break;
+			if(ii>=countt){
+				break;
+			}		
 		}
 	}
 
 	int length = v.size();
 
-	if(length==2 && ( (v[0]=="PN" || v[0]=="P") && (v[1]=="V") )){
+	if(countt == 2 && length == 2 && ( (v[0]=="PN" || v[0]=="P") && (v[1]=="V") )){
 		cout << "Valid" << endl;
 	}
-	else if( (v[0]=="PN" || v[0]=="P") && (v[1]=="V" && v[2]=="N") ){
+	else if(countt == 3 && length == 3 && (v[0]=="PN" || v[0]=="P") && (v[1]=="V" && v[2]=="N") ){
 		cout << "Valid" << endl;
 	}
 	else{
 		cout << "Invalid" << endl;
-
 	}
 }
