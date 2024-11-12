39d703b5647c4194b18ed35a333abc7ae13a3896
Uttam-Sarkar <81841458+Uttam-Sarkar@users.noreply.github.com>
2024-11-12 10:59:04 +0600

    mm

--- 1.c
+++ 1.c
@@ -1,35 +1,59 @@
-// C program to implement
-// the above approach
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define RCC_CR  (*((volatile unsigned long *)0x40021000))
+#define RCC_APB2ENR  (*((volatile unsigned long *)0x40021018))
+#define RCC_CFGR  (*((volatile unsigned long *)0x40021004))
 
-// Driver code
-int main()
-{
-	FILE* ptr;
-	char ch;
+#define GPIOC_CRH  (*((volatile unsigned long *)0x40011004))
+#define GPIOC_ODR  (*((volatile unsigned long *)0x4001100C))
 
-	// Opening file in reading mode
-	ptr = fopen("test.txt", "r");
+#define SysTick_CTRL  (*((volatile unsigned long *)0xE000E010)) 
+#define SysTick_LOAD  (*((volatile unsigned long *)0xE000E014)) 
+#define SysTick_VAL  (*((volatile unsigned long *)0xE000E018)) 
 
-	if (NULL == ptr) {
-		printf("file can't be opened \n");
+void systikInit();
+void miliSecDelay();
+void delay(int time);
+void systemClockInit();
+
+int main(){
+	RCC_APB2ENR |= 0x10;
+	GPIOC_CRH &= 0xFF0FFFFF;
+	GPIOC_CRH |= 0x300000;
+	
+	
+	systikInit();
+	systemClockInit();
+	
+	
+	while(1){
+		GPIOC_ODR ^= 0x2000;		
+		delay(1000);		
 	}
+}
 
-	printf("content of this file are \n");
 
-	// Printing what is written in file
-	// character by character using loop.
-	do {
-		ch = fgetc(ptr);
-		printf("%c", ch);
 
-		// Checking if character is not EOF.
-		// If it is EOF stop reading.
-	} while (ch != EOF);
+void systikInit(){
+	SysTick_CTRL = 0;
+	SysTick_LOAD = 0x00ffffff;
+	SysTick_VAL = 0;
+	
+	SysTick_CTRL |= 5;
+}
 
-	// Closing the file
-	fclose(ptr);
-	return 0;
+void systemClockInit(){
+	RCC_CFGR &= ~(0x3); 
+	while((RCC_CFGR & (3<<2)) != (0<<2));
+
+}
+
+void miliSecDelay(){
+	SysTick_LOAD = 8000 - 1;
+	SysTick_VAL = 0;
+	while((SysTick_CTRL & 0x10000) == 0);
+}
+
+void delay(int time){
+	for(int i=0; i<time; i++){
+		miliSecDelay();
+	}
 }
